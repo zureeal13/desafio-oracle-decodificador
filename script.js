183af12eb8ab918508d5ddb6fe42f56687c26a4a
Samuel Almeida de Sá <117867963+zureeal13@users.noreply.github.com>
2024-03-18 18:01:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const inputTexto = document.querySelector(".input");
+const message = document.querySelector(".message");
+
+function btnEncriptar() {
+    const textEncriptada = encriptar(inputTexto.value);
+    message.value = textEncriptada;
+    message.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada) {
+    let matrixCode = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    for (let i = 0; i < matrixCode.length; i++) {
+        if (stringEncriptada.includes(matrixCode[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrixCode[i][0], matrixCode[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textDesencriptado = Desencriptar(inputTexto.value);
+    message.value = textDesencriptado;
+    message.style.backgroundImage = "message";
+}
+
+function Desencriptar(strDesencriptado) {
+    let matrixCode = [
+        ["a", "ai"],
+        ["e", "enter"],
+        ["i", "imes"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    strDesencriptado = strDesencriptado.toLowerCase();
+    for (let i = 0; i < matrixCode.length; i++) {
+        if (strDesencriptado.includes(matrixCode[i][0])) {
+            strDesencriptado = strDesencriptado.replaceAll(matrixCode[i][1], matrixCode[i][0]);
+        }
+    }
+    return strDesencriptado;
+}
+
+function btnCopy() {
+    const copiedText = document.querySelector(".message");
+    copiedText.select();
+    navigator.clipboard.writeText(copiedText.value);
+    copiedText.value = inputTexto.value;
+    alert("Texto copiado, enviar para a área!");
+    inputTexto.value = copiedText.value;
+    copiedText.value = "";
+    window.location.reload();
+}
